395019e62608077388b8c2021b2a1a8a21946b8e
BerkovichPeter <p.berckowi42013@gmail.com>
2021-11-11 23:19:28 +0300

    Added Matrix solving

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy
 import numpy as np
 
 Matrix = [[4, -1, 0, -1, 0, 0, 0, 0, 0],
@@ -13,3 +14,13 @@ Matrix = [[4, -1, 0, -1, 0, 0, 0, 0, 0],
 
 plt.spy(Matrix)
 plt.show()
+Solv = [0.0625,
+        0.0625,
+        0.0625,
+        0.0625,
+        0.0625,
+        0.0625,
+        0.0625,
+        0.0625,
+        0.0625]
+print(np.linalg.solve(numpy.array(Matrix), numpy.array(Solv)))
